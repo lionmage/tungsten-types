ca1caf1d2cb48579d76fd3ee9aca72c5e43490d4
Robert Poole <Tarquin.AZ@gmail.com>
2023-04-01 15:42:12 -0700

    Added comments, links.

--- src/main/java/tungsten/types/functions/impl/CubicSpline2D.java
+++ src/main/java/tungsten/types/functions/impl/CubicSpline2D.java
@@ -9,14 +9,29 @@ import tungsten.types.util.MathUtils;
 /**
  * A representation of a cubic spline in 2 dimensions, intended for curve-fitting and
  * other applications where cubic splines may be appropriate.  For now, the input
- * argument is assumed to be a variable named <em>x</em>.<br/>
+ * argument is assumed to be a variable named <em>x</em>. A typical application
+ * might involve multiple splines contained by a {@link PiecewiseFunction} with
+ * {@link tungsten.types.functions.impl.RealPiecewiseFunction.SmoothingType#NONE SmoothingType = NONE}.<br/>
  * The spline function is of the form:<br/>
  * S(x) = a + b(x&minus;x<sub>0</sub>) + c(x&minus;x<sub>0</sub>)<sup>2</sup> + d(x&minus;x<sub>0</sub>)<sup>3</sup>
+ * @see <a href="https://en.wikipedia.org/wiki/Spline_(mathematics)">the Wikipedia article on splines,
+ *  which also gives excellent details on a basic algorithm for computing natural cubic splines</a>
+ * @see PiecewiseFunction
  */
 public class CubicSpline2D extends UnaryFunction<RealType, RealType> {
     private final RealType a, b, c, d;
     private final Range<RealType> scope;
 
+    /**
+     * Creates a cubic spline with the listed coefficients.<br/>
+     * These coefficients directly relate to the spline equation
+     * a&nbsp;+&nbsp;b&sdot;(x&minus;x<sub>0</sub>)&nbsp;+&nbsp;c&sdot;(x&minus;x<sub>0</sub>)<sup>2</sup>&nbsp;+&nbsp;d&sdot;(x&minus;x<sub>0</sub>)<sup>3</sup>
+     * @param a     the first spline parameter
+     * @param b     the second spline parameter
+     * @param c     the third spline parameter
+     * @param d     the fourth spline parameter
+     * @param scope the {@link Range} over which this spline is defined
+     */
     public CubicSpline2D(RealType a, RealType b, RealType c, RealType d, Range<RealType> scope) {
         super("x");
         this.a = a;
