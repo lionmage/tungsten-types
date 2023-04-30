2a33f056b268d423553579036deb54dc0a86f042
Robert Poole <Tarquin.AZ@gmail.com>
2023-04-30 02:25:48 -0700

    Copyright and license.

--- src/main/java/tungsten/types/numerics/impl/Phi.java
+++ src/main/java/tungsten/types/numerics/impl/Phi.java
@@ -1,4 +1,27 @@
 package tungsten.types.numerics.impl;
+/*
+ * The MIT License
+ *
+ * Copyright © 2023 Robert Poole <Tarquin.AZ@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import tungsten.types.Numeric;
 import tungsten.types.Set;
@@ -19,6 +42,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * The Golden Ratio, denoted by &#x03D5; (the Greek letter phi).
  * Phi is perhaps not as important as Euler's number or &pi;,
  * but it shows up in a lot of places.
+ * @author Robert Poole, <a href="mailto:tarquin@alum.mit.edu">MIT alumni e-mail</a> or
+ *  <a href="mailto:Tarquin.AZ@gmail.com">Gmail</a>
  * @see tungsten.types.set.impl.FibonacciNumbers
  * @see <a href="https://en.wikipedia.org/wiki/Phi">the Wikipedia article on Phi</a>
  */
