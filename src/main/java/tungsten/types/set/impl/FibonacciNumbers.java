b53d47d6a9057c8c55a480e4455fee19bc182d99
Robert Poole <Tarquin.AZ@gmail.com>
2023-04-30 10:40:36 -0700

    License and Javadoc updates.

--- src/main/java/tungsten/types/set/impl/FibonacciNumbers.java
+++ src/main/java/tungsten/types/set/impl/FibonacciNumbers.java
@@ -1,4 +1,27 @@
 package tungsten.types.set.impl;
+/*
+ * The MIT License
+ *
+ * Copyright © 2023 Robert Poole <Tarquin.AZ@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import tungsten.types.Numeric;
 import tungsten.types.Range;
@@ -27,6 +50,8 @@ import java.util.stream.StreamSupport;
  * Note that this set does not contain zero (0) as the algorithm starts computation
  * with 1, 1&hellip;
  * <br/> The first {@link #MAX_N_TO_CACHE} generated values are cached for performance.
+ * @author Robert Poole, <a href="mailto:tarquin@alum.mit.edu">MIT alumni e-mail</a>
+ *  or <a href="mailto:Tarquin.AZ@gmail.com">Gmail</a>
  */
 public class FibonacciNumbers implements Set<IntegerType> {
     public static final String EPSILON_LIMIT = "tungsten.types.set.impl.FibonacciNumbers.epsilonLimit";
