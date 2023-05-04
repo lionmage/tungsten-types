1e5bd73ea437696c9e1997e904649c523c043e8f
Robert Poole <Tarquin.AZ@gmail.com>
2023-05-04 06:44:11 -0700

    A couple more tests surrounding the Wilson matrix. Also tests interoperability of different Matrix implementations.

--- src/test/java/tungsten/types/matrix/impl/MatrixValidationTest.java
+++ src/test/java/tungsten/types/matrix/impl/MatrixValidationTest.java
@@ -24,6 +24,7 @@ public class MatrixValidationTest {
     IntegerType two = new IntegerImpl(BigInteger.valueOf(2L), true);
     Matrix<RealType> antidiagonal;
     Matrix<IntegerType> W;  // Wilson matrix
+    Matrix<IntegerType> Z;  // factorization of W
     // source: https://en.wikipedia.org/wiki/Wilson_matrix
     String[][] wilsonEntries = {
             {"5", "7", "6", "5"},
@@ -31,17 +32,26 @@ public class MatrixValidationTest {
             {"6", "8", "10", "9"},
             {"5", "7", "9", "10"}
     };
+    String[][] wilsonFactorEntries = {
+            {"2", "3", "2", "2"},
+            {"1", "1", "2", "1"},
+            {"0", "0", "1", "2"},
+            {"0", "0", "1", "1"}
+    };
 
     public MatrixValidationTest() {
         RealType[][] src = new RealType[][] {{zero, one}, {one.negate(), zero}};
         antidiagonal = new BasicMatrix<>(src);
         IntegerType[][] iSrc = new IntegerType[4][4];
+        IntegerType[][] iSrc2 = new IntegerType[4][4];
         for (int j = 0; j < wilsonEntries.length; j++) {  // row
             for (int k = 0; k < wilsonEntries[0].length; k++) { // column
                 iSrc[j][k] = new IntegerImpl(wilsonEntries[j][k]);
+                iSrc2[j][k] = new IntegerImpl(wilsonFactorEntries[j][k]);
             }
         }
         W = new BasicMatrix<>(iSrc);
+        Z = new ColumnarMatrix<>(iSrc2);
     }
 
     @Test
@@ -64,9 +74,16 @@ public class MatrixValidationTest {
     public void wilsonMatrix() {
         IntegerType det = W.determinant();
         assertTrue(One.isUnity(det), "The determinant of Wilson's matrix should be 1");
+        // first row
         RowVector<IntegerType> testRow = new ArrayRowVector<>(new IntegerImpl("68"),
                 new IntegerImpl("-41"), new IntegerImpl("-17"), new IntegerImpl("10"));
+        // third row
+        RowVector<IntegerType> testRow2 = new ArrayRowVector<>(new IntegerImpl("-17"),
+                new IntegerImpl("10"), new IntegerImpl("5"), new IntegerImpl("-3"));
         Matrix<? extends Numeric> Winv = W.inverse();
         assertEquals(testRow, Winv.getRow(0L));
+        assertEquals(testRow2, Winv.getRow(2L));
+        // check the factorization
+        assertEquals(W, Z.transpose().multiply(Z));
     }
 }
