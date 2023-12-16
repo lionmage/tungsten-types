9400765d42f60422621687039ff33bab410afbe2
Robert Poole <Tarquin.AZ@gmail.com>
2023-12-15 21:02:42 -0700

    Adding package docs where they might be useful.

---
+++ src/main/java/tungsten/types/util/ingest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains parsers to ingest text files and
+ * generate data structures from them, e.g. matrices.
+ * @since 0.3
+ * @author Robert Poole, <a href="mailto:tarquin@alum.mit.edu">MIT alumni e-mail</a>
+ *   or <a href="mailto:Tarquin.AZ@gmail.com">Gmail</a>
+ */
+package tungsten.types.util.ingest;
---
+++ src/main/java/tungsten/types/util/rendering/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains helper logic and data type-specific rendering strategies
+ * for rendering data structures (e.g., matrices) textually.
+ * @since 0.1
+ * @author Robert Poole, <a href="mailto:tarquin@alum.mit.edu">MIT alumni e-mail</a>
+ *   or <a href="mailto:Tarquin.AZ@gmail.com">Gmail</a>
+ */
+package tungsten.types.util.rendering;
