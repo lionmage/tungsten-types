809d635cb3eb4aa37149cbaf4e171fa5acc3ada5
Robert Poole <Tarquin.AZ@gmail.com>
2024-07-24 17:17:40 -0700

    Switched to semicolons and added a constructor that takes an explicit Class argument.

--- src/main/java/tungsten/types/functions/ArgMap.java
+++ src/main/java/tungsten/types/functions/ArgMap.java
@@ -45,7 +45,7 @@ public class ArgMap<T extends Numeric> extends HashMap<String, T> {
      * populates this {@link Map} with one or more variable
      * mappings parsed from the input.
      * <br>
-     * The accepted format is: [a:1,b:5,c:2.7]
+     * The accepted format is: [a:1;b:5;c:2.7]
      * <br>
      * Square brackets are optional.
      *
@@ -53,12 +53,48 @@ public class ArgMap<T extends Numeric> extends HashMap<String, T> {
      */
     public ArgMap(String init) {
         super();
+        // it would be nice to use the 2-arg version of this constructor and just pass in the Class
+        // obtained from ClassTools.getTypeArguments(), but among other things, getClass() can't be
+        // called before you call the superclass constructor
         final Class<T> clazz = (Class<T>) ClassTools.getTypeArguments(HashMap.class, getClass()).get(0);
         String stripped = init.strip();
         if (stripped.startsWith("[") && stripped.endsWith("]")) {
             stripped = stripped.substring(1, stripped.length() - 1);
         }
-        String[] mappings = stripped.split("\\s*,\\s*");
+        String[] mappings = stripped.split("\\s*;\\s*");
+        for (String mapping : mappings) {
+            int colonPos = mapping.indexOf(':');
+            if (colonPos < 1) throw new IllegalArgumentException("Bad mapping format: " + mapping);
+            final String varName = mapping.substring(0, colonPos).strip();
+            String strValue = mapping.substring(colonPos + 1);
+            put(varName, OptionalOperations.dynamicInstantiate(clazz, strValue));
+        }
+    }
+
+    /**
+     * Construct a map of var names to bound values.
+     * This constructor takes a formatted {@link String} and
+     * a {@code Class<T>} to explicitly set the element type,
+     * then populates this {@link Map} with one or more variable
+     * mappings parsed from the input.
+     * <br>
+     * The accepted format is: [a:1;b:5;c:2.7]<br>
+     * Semicolons are used to delimit each mapping to avoid
+     * issues with different characters being used as decimal
+     * points or grouping separators in different locales.
+     * <br>
+     * Square brackets are optional.
+     *
+     * @param init  the formatted string specifying variable mappings
+     * @param clazz the {@code Numeric} type of this map's elements
+     */
+    public ArgMap(String init, Class<T> clazz) {
+        super();
+        String stripped = init.strip();
+        if (stripped.startsWith("[") && stripped.endsWith("]")) {
+            stripped = stripped.substring(1, stripped.length() - 1);
+        }
+        String[] mappings = stripped.split("\\s*;\\s*");
         for (String mapping : mappings) {
             int colonPos = mapping.indexOf(':');
             if (colonPos < 1) throw new IllegalArgumentException("Bad mapping format: " + mapping);
