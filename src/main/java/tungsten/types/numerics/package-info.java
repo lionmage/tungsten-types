b49136d67bbc6594cea3e600277a340731dfcd45
Robert Poole <Tarquin.AZ@gmail.com>
2025-02-02 12:34:54 -0700

    Package documentation.

--- src/main/java/tungsten/types/numerics/package-info.java
+++ src/main/java/tungsten/types/numerics/package-info.java
@@ -3,6 +3,31 @@
  * for the subtypes that are supported, as well as concrete implementations in
  * {@link tungsten.types.numerics.impl}.  Currently, the supported numeric types
  * are integer, rational, real, and complex.<br>
+ * Some numeric types have multiple implementations.  For example:
+ * <ul>
+ *     <li>{@link tungsten.types.numerics.ComplexType} has 2 general implementations and 1 constant:
+ *     <ul>
+ *         <li>{@link tungsten.types.numerics.impl.ComplexRectImpl} represents complex numbers
+ *           internally as a rectangular pair of real and imaginary components</li>
+ *         <li>{@link tungsten.types.numerics.impl.ComplexPolarImpl} uses a polar
+ *           representation consisting of a modulus (magnitude) and an argument (angle)</li>
+ *         <li>{@link tungsten.types.numerics.impl.ImaginaryUnit} is a constant representing&nbsp;&#x2148;</li>
+ *     </ul>
+ *     </li>
+ *     <li>{@link tungsten.types.numerics.RealType} has multiple implementations and is the
+ *       base type of multiple constants.  Some examples:
+ *       <ul>
+ *           <li>{@link tungsten.types.numerics.impl.RealImpl} is the workhorse for real values</li>
+ *           <li>{@link tungsten.types.numerics.impl.ContinuedFraction} provides a representation
+ *             of real values as a sequence of {@code long}-valued terms, and offers a way to
+ *             precisely represent many irrational values (e.g., quadratic irrationals, Euler's number)</li>
+ *           <li>{@link tungsten.types.numerics.impl.Pi} provides an arbitrarily precise approximation
+ *             of &pi; to as many decimal places as {@code MathContext} supports</li>
+ *           <li>{@link tungsten.types.numerics.impl.RealInfinity} provides a real representation
+ *             of +&infin; and &minus;&infin;</li>
+ *       </ul>
+ *     </li>
+ * </ul>
  * This package also includes two enums necessary to support these types,
  * {@link tungsten.types.numerics.Sign} (which encapsulates whether a value is
  * negative or positive) and {@link tungsten.types.numerics.NumericHierarchy}
--- src/main/java/tungsten/types/util/package-info.java
+++ src/main/java/tungsten/types/util/package-info.java
@@ -2,14 +2,16 @@
  * A package containing multiple utility classes and helper classes.<br>
  * Utility classes include:
  * <ul>
- *     <li>{@link tungsten.types.util.ClassTools} covering low-level class operations</li>
- *     <li>{@link tungsten.types.util.OptionalOperations}, which contains methods to perform some common operations
+ *     <li>{@link tungsten.types.util.ClassTools} covers low-level class operations</li>
+ *     <li>{@link tungsten.types.util.OptionalOperations} contains methods to perform some common operations
  *       on different types</li>
- *     <li>{@link tungsten.types.util.UnicodeTextEffects} embodying all things Unicode related to rendering
+ *     <li>{@link tungsten.types.util.UnicodeTextEffects} embodies all things Unicode related to rendering
  *       various data types in text</li>
+ *     <li>{@link tungsten.types.util.ANSITextEffects} provides text effects such as bold, italic, and
+ *       colored highlighting</li>
  *     <li>{@link tungsten.types.util.RangeUtils}, which contains methods to generate and manipulate
  *       {@link tungsten.types.Range}s</li>
- *     <li>{@link tungsten.types.util.MathUtils} providing basic functions, including trigonometry, and various
+ *     <li>{@link tungsten.types.util.MathUtils} provides basic functions, including trigonometric, and various
  *       other operations, such as matrix decomposition</li>
  * </ul>
  * Helper classes include:
@@ -20,6 +22,11 @@
  *     <li>{@link tungsten.types.util.CombiningIterator}, an {@link java.util.Iterator} designed to combine two
  *       iterations into one</li>
  *     <li>{@link tungsten.types.util.LRUCache}, a least-recently-used cache with configurable size</li>
+ *     <li>{@link tungsten.types.util.GosperTermIterator}, used for arithmetic operations on continued
+ *       fractions by consuming two {@code Iterator<Long>} instances and returning a third</li>
+ *     <li>{@link tungsten.types.util.RationalCFTermAdapter}, used for incorporating rational values in
+ *       continued fraction operations, presenting terms as an {@code Iterator<Long>}</li>
+ *     <li>{@link tungsten.types.util.CFCleaner}, used for cleaning up in-flight continued fraction terms</li>
  * </ul>
  * The {@link tungsten.types.util.rendering} package contains textual rendering strategies for various data types.
  *
