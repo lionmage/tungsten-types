4f70180a2cd33e84fb805b5ca163b860d6daabe4
Robert Poole <Tarquin.AZ@gmail.com>
2026-02-22 23:14:38 -0700

    Added a test case.

--- src/test/java/tungsten/types/numerics/impl/PiTest.java
+++ src/test/java/tungsten/types/numerics/impl/PiTest.java
@@ -104,6 +104,9 @@ public class PiTest {
         
         numtype = ComplexType.class;
         assertTrue(instance.isCoercibleTo(numtype));
+
+        numtype = RationalType.class;
+        assertFalse(instance.isCoercibleTo(numtype));
     }
 
     /**
